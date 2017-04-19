28d47e8ceff9b9e3c861d0515e53ae654b97ea0a
Eric Bitikofer <e.bitikofer@gmail.com>
2017-04-18 22:58:22 -0400

    added functions to my ocaml library

--- f.ml
+++ f.ml
@@ -9,43 +9,28 @@ let rec unique_el el lst =
   match lst with
     [] -> []
     | hd::tl when hd <> el -> hd::unique_el el tl
-    | hd::tl when hd = el -> el::(remove_el el tl)
-    | hd::_ when hd = el -> []
+    | hd::tl when hd = el -> el::remove_el el tl
 and remove_el el lst =
   match lst with
     [] -> []
     | hd::tl when hd <> el -> hd::remove_el el tl
-    | hd::_ when hd = el -> remove_el el tl
+    | hd::tl when hd = el -> remove_el el tl
 ;;
 
-(*
 let rec find_multiples lst =
   match lst with
     [] -> []
-    | first::second::tl -> unique_el first second::tl find_multiples(x.tail)
-    | hd::_ -> hd::[]
-
-and unique el lst =
+    | hd::tl when member hd tl = false -> find_multiples (remove_el hd tl)
+    | hd::tl when member hd tl = true -> hd::find_multiples (remove_el hd tl)
+and member el lst =
   match lst with
-    [] -> []
-    | hd::tl when hd <> el -> hd::unique el tl
-    | hd::tl when hd = el -> el::remove_l tl
-    | hd::_ when hd <> el -> hd::[]
-    | hd::_ when hd = el -> []
-and remove_l el lst =
-  match lst with
-    [] -> []
-    | hd::tl when hd <> el -> hd::remove_l el tl
-    | hd::tl when hd = el -> remove el tl
-    | hd::_ when hd <> el -> hd::[]
-    | hd::_ when hd = el -> []
+    [] -> false
+    | hd::tl when hd <> el -> member el tl
+    | hd::tl when hd = el -> true
 ;;
-*)
-(*
+
 let rec keep_two lst =
   match lst with
     [] -> []
-    | if item is not in single add to single and add to final
-    | if item is not in double add to double and add to final
-    | if item is in double continue
-*)
+    | hd::tl -> hd::unique_el hd (keep_two tl)
+;;
